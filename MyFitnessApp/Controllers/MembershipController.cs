8f66ba149c24ad0c39423b92d9dd2ad065d370d8
Lisa <lisajbalbach@gmail.com>
2020-01-19 13:50:18 -0500

    Add project files.

---
+++ MyFitnessApp/Controllers/MembershipController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFitnessApp.Models;
+
+
+namespace MyFitnessApp.Controllers
+{
+    public class MembershipController : Controller
+    {
+        // GET: Membership
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MemberInfo(int memID, string memName, int memAge, string memGender)
+        {
+            Members m = new Members()
+            {
+                ID = memID,
+                name = memName,
+                gender = memGender,
+                age = memAge
+            };
+            return View("MemberConfirm", m);
+        }
+        public ActionResult MemberInfo()
+        {
+            return View();
+        }
+        public ActionResult BMICalculator()
+        {
+            return View();
+        }
+        public ActionResult MemberList()
+        {
+            return View();
+        }
+        public ActionResult FeatureSuggestionForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFitnessApp/Models/BMI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFitnessApp.Models
+{
+    public class BMI
+    {
+        public int ID { get; set; }
+        public DateTime statDate { get; set; } = DateTime.Now;
+        public float weight { get; set; } = 150;
+        public float height { get; set; } = 65;
+        public float BMICalculation { get; set; } = 0;
+    }
+}
